6835602d66c226a3ac38d747f7fec46deaff5554
alealejo25 <alejandrogianuzzi@gmail.com>
2021-07-23 22:55:55 -0300

    cambio

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -99,7 +99,7 @@
           <span class="logo-lg"><b>Trans. Lavalle</b></span>
         </a>
 
-        <!-- Header Navbar: style can be found in header.less -->
+         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top" role="navigation">
           <!-- Sidebar toggle button-->
           <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
@@ -122,32 +122,21 @@
                     <ul >
                         <!-- Authentication Links -->
                         @guest
-                            <li >
+                           
                                 <a class="nav-link" href="{{ route('login') }}" >{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrar') }}</a>
-                                </li>
-                            @endif
+                           
+                           
                         @else
-                            <li class="nav-item dropdown">
+
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Salir') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                   <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
-                                </div>
-                            </li>
+                        
+
                         @endguest
                     </ul>
                 </div>
@@ -156,33 +145,13 @@
                 <ul>
                 
                   @auth
-                     {{ Auth::user()->name }}
-                  @endauth
-
-
-                  @auth
-                   <li>
+                  
                         <a href="#" onclick="event.preventDefault();
                           document.getElementById('logout-form').submit();">Cerrar Sesion</a>
-                      </li>
-                   @else
-                        <li >  <a href="{{ route('login') }}" >Login</a>    <li>
+
                   @endauth
                 </ul>
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                </form>
-                  <!-- Menu Footer-->
-                  <li class="user-footer">
-                    
-                    <div class="pull-right">
-                     <!--  <a href="#" class="btn btn-default btn-flat">Cerrar</a> -->
-                    </div>
-                  </li>
-                </ul>
-              </li>
-              
+          
             </ul>
           </div>
 
